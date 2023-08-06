05532d80ed08dc9a3bed5f64ce549f3aad6c592c
bober2005 <127338926+bober2005@users.noreply.github.com>
2023-08-06 12:49:58 +0300

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+function createGalleryItem(item) {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+          />
+        </a>
+      </li>
+    `;
+  }
+  
+  function renderGallery() {
+    const galleryList = document.querySelector('.gallery');
+    const galleryItemsMarkup = galleryItems.map(createGalleryItem).join('');
+    galleryList.innerHTML = galleryItemsMarkup;
+  }
+  
+
+  renderGallery();
+  const galleryList = document.querySelector('.gallery');
+galleryList.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+  const target = event.target;
+
+  if (target.classList.contains('gallery__image')) {
+    const largeImageURL = target.dataset.source;
+    const instance = basicLightbox.create(`<img src="${largeImageURL}" alt="${target.alt}">`);
+    instance.show();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+
+function createGalleryItem(item) {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>
+  `;
+}
+
+
+function renderGallery() {
+  const galleryList = document.querySelector('.gallery');
+  const galleryItemsMarkup = galleryItems.map(createGalleryItem).join('');
+  galleryList.innerHTML = galleryItemsMarkup;
+}
+
+renderGallery();
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: "alt",
+  captionDelay: 250,
+});
